e8fc0eaded4752ff6ae908094b9ab7c6b2af7343
Nandrian <ranarkai@gmail.com>
2020-10-15 15:59:30 +0300

    Ajout file JS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// CALCUL SURFACE D'UN CARRE
+let coteCarre = 3
+let surfaceCarre = coteCarre * coteCarre
+
+console.log("SURFACE = "+ surfaceCarre)
+
+let larg = 4
+let long = 5
+let surface = larg * long
+
+console.log("SURFACE = "+ surface)
